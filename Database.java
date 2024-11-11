d4279ee1f4269c48937edb48d5777f36b0901c38
Tsvetelin Marinov <cvetelinmarinov566@gmail.com>
2024-11-11 17:10:12 +0200

    Add files via upload

---
+++ Database.java
@@ -0,0 +1,70 @@
+/**
+* Main database
+*/
+
+
+
+package appdata;
+
+
+
+
+
+
+
+
+import java.util.HashMap;
+
+
+
+
+
+
+
+
+
+/**
+*
+*/
+public class Database {
+	
+	
+	
+	
+	/**
+	 * Create the database
+	 */
+	private HashMap<String,String> database = new HashMap<String,String>();
+	
+	
+	
+	
+	
+	
+	/**
+	 * 
+	 */
+	Database(){
+		
+		database.put("Cvetelinm", "1234");
+		database.put("Cvetelinmarinov","4321");
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/**
+	 * Get users data
+	 */
+	public HashMap<String,String> get_users_data(){ return database; }
+	
+	
+	
+	
+}
